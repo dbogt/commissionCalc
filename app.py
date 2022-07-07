0b30ba362ccec163f3632d863ac11da1bf575740
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-06 22:25:55 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+st.header("Loan Advance Calculator")
+start = st.date_input("Date of Advancement")
+end = st.date_input("Date of Deal Close")
+advance = st.number_input("Amount to be advanced", value=1000)
+delta = (end - start).dt.days
+st.write(delta)
+
+fee = 0.75 * advance / 1000 * delta + 50
+st.write("Fee is ${:,.2f}".format(fee))
+
