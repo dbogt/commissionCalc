82e70958c96babb2458c11dac664f2813dd912ae
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-06 22:30:59 -0400

    fix

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ start = st.date_input("Date of Advancement")
 end = st.date_input("Date of Deal Close")
 advance = st.number_input("Amount to be advanced", value=1000)
 delta = (end - start).days
-st.write(delta)
+st.write("Days until close: {}".format(delta))
 
 fee = 0.75 * advance / 1000 * delta + 50
-st.write("Fee is ${:,.2f}".format(fee))
+st.write("Borrow fee to be paid is ${:,.2f}".format(fee))
 
