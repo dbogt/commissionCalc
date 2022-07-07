08491b6c537dc1167616d444e8e7f4e3b2ccd56f
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-06 22:34:08 -0400

    sidebar

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import pandas as pd
 import streamlit as st
 
 st.header("Loan Advance Calculator")
-start = st.date_input("Date of Advancement")
-end = st.date_input("Date of Deal Close")
-advance = st.number_input("Amount to be advanced", value=1000)
+start = st.sidebar.date_input("Date of Advancement")
+end = st.sidebar.date_input("Date of Deal Close")
+advance = st.sidebar.number_input("Amount to be advanced", value=1000)
 delta = (end - start).days
-st.write("Days until close: {}".format(delta))
+st.sidebar.write("Days until close: {}".format(delta))
 
 fee = 0.75 * advance / 1000 * delta + 50
-st.metric("Borrow fee to be paid is", "${:,.2f}".format(fee))
+st.sidebar.metric("Borrow fee to be paid is", "${:,.2f}".format(fee))
 
