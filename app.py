2a2d966a1dfe196e33af4e7453fd522830edd235
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-06 22:39:34 -0400

    remove sidebar

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import streamlit as st
 
-st.sidebar.header("Loan Advance Calculator")
-start = st.sidebar.date_input("Date of Advancement")
-end = st.sidebar.date_input("Date of Deal Close")
-advance = st.sidebar.number_input("Amount to be advanced", value=1000)
+st.header("Loan Advance Calculator")
+start = st.date_input("Date of Advancement")
+end = st.date_input("Date of Deal Close")
+advance = st.number_input("Amount to be advanced", value=1000)
 delta = (end - start).days
-st.sidebar.write("Days until close: {}".format(delta))
+st.write("Days until close: {}".format(delta))
 
 fee = 0.75 * advance / 1000 * delta + 50
-st.sidebar.metric("Borrow fee to be paid is", "${:,.2f}".format(fee))
+st.metric("Borrow fee to be paid is", "${:,.2f}".format(fee))
 
